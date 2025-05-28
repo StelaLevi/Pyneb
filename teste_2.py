dd05c59899513e80a714e3f334bb97454ab144aa
StelaLevi <165806038+StelaLevi@users.noreply.github.com>
2025-05-28 20:16:03 -0300

    teste_2.py

--- teste_2.py
+++ teste_2.py
@@ -58,5 +58,32 @@ plt.show()
 Te, Ne = diags.getCrossTemDen('[NII] 5755/6548', '[SII] 6731/6716', obs=obs)
 print('Te = {0:5.0f} K, Ne = {1:7.1f} cm-1'.format(Te, Ne))
 
+# Atom creation and definition of physical conditions 
+o3=pn.Atom('O', 3)
+tem=np.arange(100)*300+300
+den = Ne
 
+# Comment the second if you want all the lines to be plotted
+lineList=o3.lineList
+lineList=[4363, 4959, 5007]
+
+# Plot	
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_ylim([1.e-30, 5e-20])
+
+for line in lineList:
+    y=o3.getEmissivity(tem, den, wave=line)
+    plt.semilogy(tem, y,  label="{:.0f}".format(line))
+
+plt.xlabel('T$_e$ [K]')
+plt.ylabel("j(T) [erg cm$^{-3}$ s${-1}$]")
+plt.legend(loc='lower right')
+plt.title('[O III] emissivities @ N$_e$={:.0f}'.format(den))
+plt.show()
+
+obs = pn.Observation()
+obs.readData('obs.dat', fileFormat='lines_in_rows', err_default=0.05) # fill obs with data read from observations1.dat
+obs.def_EBV(label1="H1r_6563A", label2="H1r_4861A", r_theo=2.85)
+obs.correctData(normWave=4861.)
 
